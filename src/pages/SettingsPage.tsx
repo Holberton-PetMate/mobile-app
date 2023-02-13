83b0cd8a3f98b038b80ade7530dc98021ed39e7d
mattowsh <mattobelen@gmail.com>
2023-02-13 15:12:48 -0300

    Settings page > modal component > children advanced

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -2,18 +2,21 @@ import SettingsList from "../components/SettingsList";
 import PageHeading from "../components/PageHeading";
 import ModalCard from "../components/ModalCard";
 import "../styles/components/modalCard-children.css";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-
+import Clock from "../components/Clock";
 
 const SettingsPage: React.FC = () => {
   return (
     <>
       <PageHeading backUrl="/my-petmates" title="Settings" />
       <SettingsList />
+
       <ModalCard action="create" title="New feeding time">
         <div className="modal-container">
           <div className="time-selector">
             <h3 className="children-title">Time</h3>
+            <div className="clock-container">
+              <Clock />
+            </div>
           </div>
           <div className="quantity-selector">
             <div className="quantity-title">
