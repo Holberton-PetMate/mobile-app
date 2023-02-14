b9d6428f0e0f7b166f9539c7f0c5c20c3ebb4a0c
Ignacio Castro <ignaciocastro347@gmail.com>
2023-02-14 16:00:59 -0300

    Fixes

--- src/components/ModalCard.tsx
+++ src/components/ModalCard.tsx
@@ -5,20 +5,25 @@ import H3 from "./fonts/H3";
 interface ModalCardProps extends PropsWithoutRef<JSX.IntrinsicElements["div"]> {
 	action?: "create" | "update";
 	title?: string
+	onSubmit?: () => void
 }
 
-const ModalCard = ({ action, title, children, ...rest }:ModalCardProps) => {
+const ModalCard = ({ action, title, onSubmit, children, ...rest }: ModalCardProps) => {
 
 	return (
-		<div className="general-container" { ...rest }>
+		<div className="general-container" {...rest}>
 			<div className="grey-line"></div>
 			<div className="action-title">
 				<H3>{title}</H3>
 			</div>
-			{children}
-			<button className="action-buttom">
-				{action}
-			</button>
+			<div className="childrenContainer">
+				{children}
+			</div>
+			<div className="buttonsContainer">
+				<button className="action-buttom" onClick={onSubmit}>
+					{action}
+				</button>
+			</div>
 		</div>
 	);
 };
--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,36 +1,68 @@
 import SettingsList from "../components/SettingsList";
 import PageHeading from "../components/PageHeading";
 import ModalCard from "../components/ModalCard";
-import "../styles/components/modalCard-children.css";
-import Clock from "../components/Clock";
+import { TextField, InputAdornment } from "@mui/material";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 const SettingsPage: React.FC = () => {
-  return (
-    <>
-      <PageHeading backUrl="/my-petmates" title="Settings" />
-      <SettingsList />
+	const [time, setTime] = useState<string>(dayjs().format("HH:mm"));
+	const [quantity, setQuantity] = useState<number | null>(null);
 
-      <ModalCard action="create" title="New feeding time" style={{minHeight: "400px"}}>
-        <div className="modal-container">
-          <div className="time-selector">
-            <h3 className="children-title">Time</h3>
-            <div className="clock-container">
-              <Clock />
-            </div>
-          </div>
-          <div className="quantity-selector">
-            <div className="quantity-title">
-              <h3 className="children-title">Quantity of food</h3>
-            </div>
-            <div className="input-container">
-              <input className="quantity-input"></input>
-              <span>gr.</span>
-            </div>
-          </div>
-        </div>
-      </ModalCard>
-    </>
-  );
+	const validateQuantity = (e: any) => {
+		const value = e.target.value;
+		setQuantity(value)
+		if (value === undefined) return
+		if (value < 0) {
+			e.target.value = 0;
+		} else if (value > 500) {
+			e.target.value = 500;
+		}
+	}
+
+	const onSubmit = () => {
+		console.log({
+			time,
+			quantity
+		})
+	}
+
+	return (
+		<>
+			<PageHeading backUrl="/my-petmates" title="Settings" />
+			<SettingsList />
+			<ModalCard
+				action="create"
+				title="New feeding time"
+				onSubmit={onSubmit}
+				style={{ minHeight: "350px" }}
+			>
+				<TextField
+					defaultValue={time}
+					className="time"
+					label="Alarm clock"
+					type="time"
+					InputLabelProps={{ shrink: true }}
+					onChange={(e: any) => setTime(e.target.value)}
+					// 5 min
+					inputProps={{ step: 300 }}
+					sx={{ width: "100%", marginBottom: "0.5rem" }}
+				/>
+				<TextField
+					label="Quantity of food"
+					defaultValue=""
+					id="outlined-start-adornment"
+					sx={{ m: 1, width: '100%' }}
+					type="number"
+					onChange={validateQuantity}
+					InputProps={{
+						endAdornment: <InputAdornment position="end">gr.</InputAdornment>,
+						inputProps: { min: 0, max: 500 }
+					}}
+				/>
+			</ModalCard>
+		</>
+	);
 };
 
 export default SettingsPage;
