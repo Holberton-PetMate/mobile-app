5871a5aba70c1c20206d91dab031f5d7478f4073
mattowsh <mattobelen@gmail.com>
2023-02-16 13:16:32 -0300

    applied functionality to settings plus button

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -3,7 +3,10 @@ import PageHeading from "../components/PageHeading";
 import ModalCard from "../components/ModalCard";
 import { TextField, InputAdornment } from "@mui/material";
 import { useState } from "react";
+import RoundedButton from "../components/RoundedButton";
+import { Edit } from "../components/Icons"
 import dayjs from "dayjs";
+ 
 
 const SettingsPage: React.FC = () => {
 	const [time, setTime] = useState<string>(dayjs().format("HH:mm"));
@@ -32,11 +35,15 @@ const SettingsPage: React.FC = () => {
 		<>
 			<PageHeading backUrl="/my-petmates" title="Settings" />
 			<SettingsList />
+			<div className="roundedButtonContainer">
+				<RoundedButton onClick={() => setCreateModalState(true)} />
+			</div>
 			<ModalCard
 				isActive={createModalState}
 				action="create"
 				title="New feeding time"
 				onSubmit={onSubmit}
+				closeModal={() => setCreateModalState(false)}
 				style={{ minHeight: "350px" }}
 			>
 				<TextField
@@ -63,6 +70,42 @@ const SettingsPage: React.FC = () => {
 					}}
 				/>
 			</ModalCard>
+
+			{/* <div className="roundedButtonContainer">
+				<Edit onClick={() => setCreateModalState(true)} />
+			</div>
+			<ModalCard
+				isActive={createModalState}
+				action="update"
+				title="Update feeding time"
+				onSubmit={onSubmit}
+				closeModal={() => setCreateModalState(false)}
+				style={{ minHeight: "350px" }}
+			>
+				<TextField
+					defaultValue={time}
+					className="time"
+					label="Alarm clock"
+					type="time"
+					InputLabelProps={{ shrink: true }}
+					onChange={(e: any) => setTime(e.target.value)}
+					// 5 min
+					inputProps={{ step: 300 }}
+					sx={{ width: "100%", marginBottom: "0.5rem" }}
+				/>
+				<TextField
+					label="Quantity of food"
+					defaultValue=""
+					id="outlined-start-adornment"
+					sx={{ m: 1, width: '100%' }}
+					type="number"
+					onChange={validateQuantity}
+					InputProps={{
+						endAdornment: <InputAdornment position="end">gr.</InputAdornment>,
+						inputProps: { min: 0, max: 500 }
+					}}
+				/>
+			</ModalCard> */}
 		</>
 	);
 };
